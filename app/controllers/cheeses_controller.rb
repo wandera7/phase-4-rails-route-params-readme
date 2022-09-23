cf4118b2a502de1ab4f78d80082f504b36e2d218
wandera7 <julius.wandera@student.moringaschool.com>
2022-09-23 20:30:37 +0300

    Done

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -4,5 +4,9 @@ class CheesesController < ApplicationController
     cheeses = Cheese.all
     render json: cheeses
   end
+  def show
+    cheese = Cheese.find(params[:id])
+    render json: cheese
+  end
 
 end
